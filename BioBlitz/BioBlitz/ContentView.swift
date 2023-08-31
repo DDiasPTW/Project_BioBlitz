e1886aad87d8fba99fa540fb61ec98de58fb1443
Dias <59363378+DDiasPTW@users.noreply.github.com>
2023-08-31 11:19:07 +0100

    1st prototype

--- BioBlitz/BioBlitz/ContentView.swift
+++ BioBlitz/BioBlitz/ContentView.swift
@@ -1,21 +1,65 @@
-//
-//  ContentView.swift
-//  BioBlitz
-//
-//  Created by Diogo Dias on 31/08/2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var board = GameBoard()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Text("GREEN: \(board.greenScore)")
+                    .padding(.horizontal)
+                    .background(Capsule().fill(.green).opacity(board.currentPlayer == .green ? 1 : 0))
+                
+                Spacer()
+                
+                Text("BIOBLITZ")
+                
+                Spacer()
+                
+                Text("RED: \(board.redScore)")
+                    .padding(.horizontal)
+                    .background(Capsule().fill(.red).opacity(board.currentPlayer == .red ? 1 : 0))
+            }
+            .font(.system(size: 36).weight(.black))
+            
+            ZStack{
+                VStack{
+                    ForEach(0..<board.rowCount, id: \.self){ row in
+                        HStack{
+                            ForEach(0..<board.columnCount, id: \.self){ col in
+                                let bacteria = board.grid[row][col]
+                                
+                                BacteriaView(bacteria: bacteria){
+                                    board.rotate(bacteria: bacteria)
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                if let winner = board.winner{
+                    VStack{
+                        Text("\(winner) wins!")
+                            .font(.largeTitle)
+                        
+                        Button(action: board.reset){
+                            Text("Play again")
+                                .padding()
+                                .background(.green)
+                                .clipShape(Capsule())
+                        }
+                        .buttonStyle(.plain)
+                    }
+                    .padding(40)
+                    .background(.green.opacity(0.85))
+                    .cornerRadius(25)
+                    .transition(.scale)
+                }
+            }
         }
         .padding()
+        .fixedSize()
+        .preferredColorScheme(.dark)
     }
 }
 
