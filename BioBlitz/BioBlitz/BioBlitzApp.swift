afff48d6eed5c951286a01fdd3a40bae27d0f460
Dias <59363378+DDiasPTW@users.noreply.github.com>
2023-08-31 08:22:27 +0100

    Initial commit

---
+++ BioBlitz/BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Diogo Dias on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
